c84815e7856450779e9aae509f72e23ebfbabd46
swarup-prog <lswarup1234@gmail.com>
2023-01-29 15:20:43 +0545

    user state management

--- lib/responsive/responsive_screen_layout.dart
+++ lib/responsive/responsive_screen_layout.dart
@@ -1,26 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/utils/dimensions.dart';
+import 'package:provider/provider.dart';
 
-class ResponsiveLayout extends StatelessWidget {
+import '../providers/user_provider.dart';
+
+class ResponsiveLayout extends StatefulWidget {
   final Widget webScreenLayout;
   final Widget mobileScreenLayout;
 
-  const ResponsiveLayout({
-    Key? key,
-    required this.webScreenLayout,
-    required this.mobileScreenLayout
-  }) : super(key: key);
+  const ResponsiveLayout(
+      {Key? key,
+      required this.webScreenLayout,
+      required this.mobileScreenLayout})
+      : super(key: key);
+
+  @override
+  State<ResponsiveLayout> createState() => _ResponsiveLayoutState();
+}
+
+class _ResponsiveLayoutState extends State<ResponsiveLayout> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    addData();
+  }
+
+  addData() async {
+    UserProvider _userProvider = Provider.of(context, listen: false);
+    await _userProvider.refreshUser();
+  }
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-        builder: (context, constraints) {
-          if (constraints.maxWidth > webScreenSize) {
-            return webScreenLayout;
-
-          }
-          return mobileScreenLayout;
-        },
+      builder: (context, constraints) {
+        if (constraints.maxWidth > webScreenSize) {
+          return widget.webScreenLayout;
+        }
+        return widget.mobileScreenLayout;
+      },
     );
   }
 }
